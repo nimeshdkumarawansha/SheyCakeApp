264f878bdb267bc992a7848c407b3f04b40171f9
Dilshan Kumarawansha <ndkumarawansha@gmail.com>
2023-12-05 15:43:15 +0530

    product details page

--- app/src/main/java/com/orcs/sheycakeapp/Activity/MainActivity.java
+++ app/src/main/java/com/orcs/sheycakeapp/Activity/MainActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import com.orcs.sheycakeapp.R;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 private RecyclerView.Adapter adapter;
@@ -18,12 +18,5 @@ private RecyclerView recyclerViewCategoryList;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerViewCategory();
-
-    }
-    private void recyclerViewCategory() {
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        recyclerViewCategoryList = findViewById(R.id.recyclerViewCategoryList);
-        recyclerViewCategoryList.setLayoutManager(layoutManager);
     }
 }
