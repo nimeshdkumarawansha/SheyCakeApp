05475c46bd5633f3dfa1a4c99cd62c6caee7e7c6
Dilshan Kumarawansha <ndkumarawansha@gmail.com>
2023-12-08 19:12:59 +0530

    changes

--- app/src/main/java/com/orcs/sheycakeapp/Activity/LoginActivity.java
+++ app/src/main/java/com/orcs/sheycakeapp/Activity/LoginActivity.java
@@ -1,44 +1,51 @@
 package com.orcs.sheycakeapp.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.orcs.sheycakeapp.R;
 
 public class LoginActivity extends AppCompatActivity {
+    public static final String TAG = LoginActivity.class.getName();
+    private FirebaseAuth mAuth;
+    private EditText emailEditText, passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        emailEditText = findViewById(R.id.inputEmail);
+        passwordEditText = findViewById(R.id.inputPassword);
+
         findViewById(R.id.buttonGoogle).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                SignInRequest = BeginSignInRequest.builder()
-//                        .setGoogleIdTokenRequestOptions(GoogleIdTokenRequestOptions.builder()
-//                                .setSupported(true)
-//                                // Your server's client ID, not your Android client ID.
-//                                .setServerClientId(getString(R.string.default_web_client_id))
-//                                // Only show accounts previously used to sign in.
-//                                .setFilterByAuthorizedAccounts(true)
-//                                .build())
-//                        .build();
-
+                // Implement Google login here if needed
             }
         });
 
-        // login function
         findViewById(R.id.buttonLogin).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Log.i(TAG, "Login button clicked");
+                loginUser();
             }
         });
 
-        //  move register activity
         findViewById(R.id.createNewAccountText).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,4 +54,54 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void loginUser() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(LoginActivity.this, "Please enter email and password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI or perform other actions
+                            Log.i(TAG, "signInWithEmail:success");
+                            Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+
+                            // Clear input fields
+                            clearInputFields();
+
+                            // Redirect to home page
+                            redirectToHomePage();
+
+                            // You can access user details using mAuth.getCurrentUser()
+                            // For example, to get the user's email:
+                            String userEmail = mAuth.getCurrentUser().getEmail();
+                            Log.i(TAG, "User email: " + userEmail);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.i(TAG, "signInWithEmail:failure");
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    private void clearInputFields() {
+        emailEditText.getText().clear();
+        passwordEditText.getText().clear();
+    }
+
+    private void redirectToHomePage() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish(); // Optional: Close the LoginActivity to prevent going back to it using the back button
+    }
+
 }
--- app/src/main/java/com/orcs/sheycakeapp/Activity/MainActivity.java
+++ app/src/main/java/com/orcs/sheycakeapp/Activity/MainActivity.java
@@ -7,18 +7,34 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.orcs.sheycakeapp.R;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-private RecyclerView.Adapter adapter;
-private RecyclerView recyclerViewCategoryList;
+//    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewCategoryList;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseUser user = mAuth.getCurrentUser();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_main);
+
+        // Set the text of textView3 to the user's email
+        TextView textView3 = findViewById(R.id.textView3);
+        if (textView3 != null) {
+            textView3.setText("Welcome, " + mAuth.getCurrentUser().getEmail());
+        }
 
         findViewById(R.id.supportButton).setOnClickListener(new View.OnClickListener() {
             @Override
