775629215a97a4c62dda482ab24454f00abb7d6f
Dilshan Kumarawansha <ndkumarawansha@gmail.com>
2023-12-21 18:13:40 +0530

    New changes

--- app/src/main/java/com/orcs/sheycakeapp/Activity/ShowDetailsActivity.java
+++ app/src/main/java/com/orcs/sheycakeapp/Activity/ShowDetailsActivity.java
@@ -4,22 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.orcs.sheycakeapp.R;
 
 public class ShowDetailsActivity extends AppCompatActivity {
+    public static final String TAG = ShowDetailsActivity.class.getName();
+    String productId = getIntent().getStringExtra("PRODUCT_ID");
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    DocumentReference productRef = db.collection("products").document(productId);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_details);
 
-        TextView productName = findViewById(R.id.titleText);
-        productName.setText(getIntent().getStringExtra("PRODUCT_ID"));
+    TextView productNameText = findViewById(R.id.titleText);
+        productNameText.setText(getIntent().getStringExtra("PRODUCT_ID"));
 
+        productRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    // Document found, you can access the data
+                    String productName = document.getString("name");
+                    // Assuming "productName" is a field in your Firestore document
+                    // You can replace it with the actual field name you want to retrieve
+
+                    // Update your TextView
+                    productNameText.setText(productName);
+                } else {
+                }
+            } else {
+                // Handle errors
+                Exception e = task.getException();
+                Log.i(TAG,e.toString());
+            }
+        });
         findViewById(R.id.back_arrow).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
